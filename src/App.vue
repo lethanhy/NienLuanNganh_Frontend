683fe0de6577281b1431fcf8f862ea8959577019
Le Thanh Y <lethanhy107@gmail.com>
2024-02-14 10:33:35 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script>
+import ContactBook from './views/ContactBook.vue'
+import AppHeader from "@/components/AppHeader.vue";
 export default {
+  components: { 
+    ContactBook,
+  AppHeader,
+ },
 
-}
+};
 
 </script>
 
+
 <template>
-  <h1>Hello, Vue.js!</h1>
+  <div id="app">
+      <AppHeader />
+
+      <div class="container mt-3">
+            <router-view />
+      </div>
+  </div>
 </template>
+
 
 <style>
 .page {
